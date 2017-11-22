2acc39595fc4f15777f5b9f4969b52b7c97c0d14
Laurentiu Bascuti <laurentiu.bascuti96@e-uvt.ro>
2017-11-22 17:58:58 +0200

    Add files via upload

--- db_test.php
+++ db_test.php
@@ -1,10 +1,43 @@
 <?php
+
 include 'db_connection.php';
- 
+
 $conn = OpenCon();
- 
-echo "Connected Successfully";
- 
+
+echo "Conectat cu succes";
+
+function NewUser()
+{
+	$userName = $_POST['username'];
+	$password =  $_POST['psw'];
+	$email = $_POST['email'];
+	$query = "INSERT INTO accountstable (username,paswword,email) VALUES ('$userName','$password','$email')";
+	$data = mysql_query ($query)or die(mysql_error());
+	if($data)
+	{
+	echo "YOUR REGISTRATION IS COMPLETED...";
+	}
+}
+
+function SignUp()
+{
+if(!empty($_POST['username']))   //checking the 'user' name which is from Sign-Up.html, is it empty or have some text
+{
+	$query = mysql_query("SELECT * FROM accountstable WHERE userName = '$_POST[username]' AND pass = '$_POST[psw]'") or die(mysql_error());
+
+	if(!$row = mysql_fetch_array($query) or die(mysql_error()))
+	{
+		NewUser();
+	}
+	else
+	{
+		echo "SORRY...YOU ARE ALREADY REGISTERED USER...";
+	}
+}
+}
+if(isset($_POST['submit']))
+{
+	SignUp();
+}
 CloseCon($conn);
- 
 ?>
